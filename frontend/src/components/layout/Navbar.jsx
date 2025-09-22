e2f8e187ee9ca046072eecab9bfa41f2ff380c84
jsilvarabit <jvcoelho08@outlook.com>
2025-09-22 11:36:48 -0300

    Ajustes logo e nomenclaturas

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,22 +1,22 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css"; // Opcional: para estilização
-import Logo from "../../assets/images/logo.png";
+import Logo from "../../assets/images/logo_joao.png"; // Certifique-se de que o caminho está correto
 
 function Navbar() {
   return (
     <nav className="menu">
       <div className="logo">
         <Link to="/">
-          <img src={Logo} alt="Eduteka" title="Eduteka" />
+          <img src={Logo} alt="clhtech" title="clhtech" />
         </Link>
       </div>
 
       <ul>
         <li>
-          <Link to="/">Listagem</Link>
+          <Link to="/">Listagem de Usuários</Link>
         </li>
         <li>
-          <Link to="/cadastrar">Cadastrar</Link>
+          <Link to="/cadastrar">Cadastrar Usuário</Link>
         </li>
       </ul>
     </nav>
